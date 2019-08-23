8ea4076dd890fe21bcae551cb62bbfe43d1b1373
fesus rocuts <fesusrocuts@gmail.com>
2019-08-22 22:40:58 -0500

    automation and delivery

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,18 @@
+# puppet file to automation and delivery
+package {'nginx':
+  ensure  => installed,
+  name    => 'nginx'
+}
+
+file_line {'header':
+  ensure   =	> present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'add_header X-Served-By "$HOSTNAME";',
+  multiple => true
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
