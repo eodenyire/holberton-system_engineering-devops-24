92d9242c3c03f758bf12966c38bfe4811bc1e2e2
fesus rocuts <fesusrocuts@gmail.com>
2019-09-16 23:48:31 -0500

    t0 t1 t2 t3

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,24 +5,24 @@
 import requests
 import sys
 
-text1 = "Employee {} is done with tasks({}/{}):"
-url1 = 'https://jsonplaceholder.typicode.com/users/{}'
-url2 = 'https://jsonplaceholder.typicode.com/todos'
 def getrequest():
+    text1 = "Employee {} is done with tasks({}/{}):"
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'
+    url2 = 'https://jsonplaceholder.typicode.com/todos'
     users = requests.get(url1.format(sys.argv[1]))
     names = users.json().get('name')
     todos = requests.get(url2)
     todos = todos.json()
-    comp = 0
+    i = 0
     titles = []
     total = 0
     for todo in todos:
         if todo['userId'] == int(sys.argv[1]):
             if todo['completed'] is True:
-                comp += 1
+                i += 1
                 titles.append(todo['title'])
             total += 1
-    print(text1.format(names, comp, total))
+    print(text1.format(names, i, total))
     for title in titles:
         print('\t ', end="")
         print(title)
