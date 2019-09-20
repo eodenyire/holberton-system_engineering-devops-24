450b7ff62d26a0a62154d2c432e9aaa545dc336a
fesus rocuts <fesusrocuts@gmail.com>
2019-09-19 23:54:29 -0500

    t3 c0

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0. How many subs?
+"""
+import requests as r
+from sys import argv
+
+
+def count_words(subreddit, word_list, key=""):
+    """
+    queries the Reddit API and returns the number of subscribers
+    If an invalid subreddit is given, the function should return 0.
+    """
+    headers = {"User-Agent": "Frocuts"}
+    url = "http://reddit.com/r/{}/hot.json?after={}"
+    about = r.get(url.format(subreddit, key), headers=headers)
+    if about.status_code != 200:
+        print(None)
+        return 0
+    about = about.json()
+    if about["kind"] == "Listing":
+        #for data in about["data"]["children"]:
+            # print(data["data"]["title"])
+        for word in word_list:
+            print('{}: {}'.format(word, 10))
+    else:
+        print(None)
