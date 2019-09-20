9bc5f23be96e4c438463ab08c74f0598f5dca92c
fesus rocuts <fesusrocuts@gmail.com>
2019-09-19 22:22:12 -0500

    t1 c1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0. How many subs?
+"""
+import requests as r
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    If an invalid subreddit is given, the function should return 0.
+    """
+    headers = {"User-Agent": "APILearning"}
+    url = "http://reddit.com/r/{}/hot.json?limit=10"
+    about = r.get(url.format(subreddit), headers=headers)
+
+    if about.status_code != 200:
+        return 0
+    about = about.json()
+    if about["kind"] == "Listing":
+        for data in about["data"]["children"]:
+            print(data["data"]["title"])
+    else:
+        return 0
