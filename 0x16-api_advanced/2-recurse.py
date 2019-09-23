640f165eb5167302c859eb7621f7515a3655ecf9
fesus rocuts <fesusrocuts@gmail.com>
2019-09-22 23:50:37 -0500

    t2 c4

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,24 +11,20 @@ def recurse(subreddit, hot_list=[], key=""):
     If an invalid subreddit is given, the function should return 0.
     """
     if (subreddit is not None):
-        headers = {"User-Agent": "Frocuts"}
-        url = "http://reddit.com/r/{}/hot.json?after={}"
-        about = r.get(url.format(subreddit, key), headers=headers)
+        h = {"User-Agent": "Frocuts"}
+        url = "http://reddit.com/r/{}/hot.json"
+        p = {'after': key}
+        about = r.get(url.format(subreddit), headers=h, params=p)
         if about.status_code != 200:
             return None
         about = about.json()
-        if about["kind"] == "Listing":
-            hot_list.extend(about["data"]["children"])
-            if (len(hot_list) == 0):
-                return None
-        else:
-            return None
-    try:
-        if type(hot_list) == dict:
-            hot_list.append(hot_list[0]["data"]["title"])
-            hot_list.pop(0)
-            if about['data']['after'] is not None:
-                recurse(subreddit, hot_list, about['data']['after'])
-        return hot_list
-    except Expection:
+        # hot_list.extend(about["data"]["children"])
+        hot_list.extend([x.get('data').get('title')
+                        for x in about["data"]["children"]])
+        if about['data']['after'] is not None:
+            recurse(subreddit, hot_list, about['data']['after'])
+        if (len(hot_list) > 0):
+            return hot_list
+        return None
+    else:
         return None
