4b1f8b6ba658e95e2d31aae4c6fecc616e4eb4c2
fesus rocuts <fesusrocuts@gmail.com>
2019-10-10 22:33:38 -0500

    t0 c0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# change data 15 to 10
+exec { 'update line':
+  cwd => '/etc/default/',
+  path => ['/usr/bin/', '/bin/'],
+  commnad => 'sed -i "s/15/4096/g"'
+}
+exec { 'restart service':
+  command => '/etc/init.d/nginx restart',
+}
