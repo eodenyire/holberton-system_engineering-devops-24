5c1bd1c5869e9925b2b94b94ecafa0d98b14747e
fesus rocuts <fesusrocuts@gmail.com>
2019-10-10 23:17:30 -0500

    t0 c1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,4 @@
-# change data 15 to 10
+# change data 15 to 10240
 exec { 'update line':
-  commnad => '/usr/bin/sudo /bin/sed -i -e "s/\-n 15/\-n 10240/g" /etc/default/nginx && service nginx restart'
+  command => '/usr/bin/sudo /bin/sed -i "s/15/10240/g" /etc/default/nginx && service nginx restart'
 }
-#exec { 'restart service':
-#  cwd => '/etc/default/',
-#  command => '/etc/init.d/nginx restart',
-#  path => ['/usr/bin/', '/bin/'],
-#}
-#
-#xx -n 10000
